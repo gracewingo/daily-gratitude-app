7d0ec08ce1e806d1d5aeb5cbb0c3bd3669351179
Grace Wingo <grace.wingo@gmail.com>
2019-11-24 11:42:25 -0500

    adding env file

--- index.js
+++ index.js
@@ -3,26 +3,30 @@ const Datastore = require('nedb');
 const app = express();
 const fs = require('fs');
 require('dotenv').config()
-const request = require('request');
+const fetch = require('node-fetch');
 
-
-request('http://localhost:3000/', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) 
-  }
+app.get('/weather/:latlon', async (req, res) => {
+    const latlon  = req.params.latlon.split(",");
+    const lat = latlon[0];
+    const lon = latlon[1];
+    const gURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${process.env.API_KEY}`;
+    const fetch_response = await fetch(gURL);
+    const json = await fetch_response.json();
+    //this is a proxy server 
+    res.json(json);
+    //if we want to use the fetch api (client side api) in node, install the node-fetch package 
 })
 
 app.listen(3000, () => console.log("listening at 3000!"));
 app.use(express.static("public"));
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static("exports"));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
 app.get("/api", (req, res) => {
     database.find({}, (err, data) => {
-        if (err){ response.end(); return; }
+        if (err){ res.end(); return; }
         res.json(data);
     })
 })
@@ -34,8 +38,7 @@ app.post('/api', (request, response) => {
     let img64 = data.image;
     let dataImg = img64.replace(/^data:image\/\w+;base64,/, "");
     fs.writeFile(`public/logs/exports/all/image_${data.timestamp}.png`, dataImg, {encoding: 'base64'}, (err) => 
-        console.log('File created')
-    );
+    console.log("file created"));
 
     let image_file = `./exports/all/image_${data.timestamp}.png`;
     data.image = image_file;
--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,9 @@
 function setup(){    
     let canvas = getCanvas();
-    let lat, lon, cityState;
+    let lat, lon, cityState, mood, grateful;
     const button = document.getElementById("submit")
     const inputs = document.querySelectorAll('input');
+    //on page load/set up call the /weather endpoint  - get request
     if('geolocation' in navigator){
         console.log("geo avail")
             navigator.geolocation.getCurrentPosition(async position => {
@@ -10,18 +11,19 @@ function setup(){
                 lon = position.coords.longitude;
                 document.getElementById("latitude").textContent = lat.toFixed(2);
                 document.getElementById("longitude").textContent = lon.toFixed(2);
+                const gURL = `weather/${lat},${lon}`;
+                const response1 = await fetch(gURL);
+                const json1 = await response1.json();
+                cityState = json1.results[4].formatted_address;
             });
     } 
     button.addEventListener("click", async event => {
-        let mood = document.getElementById("mood").value;
-        let grateful = document.getElementById("grateful").value;
+        mood = document.getElementById("mood").value;
+        grateful = document.getElementById("grateful").value;
         canvas.loadPixels();
         const image = canvas.canvas.toDataURL();
-        
-        const gURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyAir9zLuptSbfJEMCZY-3LDny7dj19DCVk`
-        const results = await fetch(gURL);
-        const info = await results.json();
-        cityState = info.results[7].formatted_address;
+       //cityState = ().results[0].address_components[4].long_name + ().results[0].address_components[5].long_name
+        //cityState = data.results[7].formatted_address;
         const data = { cityState, mood, grateful, image };
         const options = {
                 method: "POST",
